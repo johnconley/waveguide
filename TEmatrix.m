c77bb3d9faf93fde972e669830e89405f3cbc1bd
John Conley <JCC282@gmail.com>
2014-04-01 20:12:48 -0400

    Simplified 3-layer TE and TM matrices

--- TEmatrix.m
+++ TEmatrix.m
@@ -1,5 +1,5 @@
 function A = TEmatrix(n0, n1, n2, a, k, kappa)
-% Amatrix - creates a matrix for the TE wave system
+% A - matrix for the TE wave system
 % 
 % n0 - 
 % n1 - 
@@ -8,63 +8,32 @@ function A = TEmatrix(n0, n1, n2, a, k, kappa)
 % k - wavenumber
 % kappa - frequency in the x direction
 %
-% Conley September 2013
+% Conley April 2014
 
 % tolerance
 tol = .06;
 
 % define the exponents
-a0 = sqrt(kappa^2-n0^2*k^2);
-a1 = sqrt(kappa^2-n1^2*k^2);
-a2 = sqrt(kappa^2-n2^2*k^2);
+a0 = sqrt(n0^2*k^2-kappa^2);
+a1 = sqrt(n1^2*k^2-kappa^2);
+a2 = sqrt(n2^2*k^2-kappa^2);
 
 % kappa roughly equals n1*k
 if (abs(kappa - n1*k) <= tol)
-    if (kappa > n0*k)
-        row1 = [1, -1, 0, 0];
-        row2 = [a0, 0, 0, 0];
-    else
-        row1 = [1, -1, 0, 0];
-        row2 = [-a0, 0, 0, 0];
-    end
-    
-    if (kappa > n2*k)
-        row3 = [0, 1, a, -exp(-a2*a)];
-        row4 = [0, 0, 1, a2*exp(-a2*a)];
-    else
-        row3 = [0, 1, a, -exp(a2*a)];
-        row4 = [0, 0, 1, -a2*exp(a2*a)];
-    end
-%     A = [1, -1, -1, 0;
-%         a0, 0, 0, 0;
-%         0, 1, a, -exp(-a2*a);
-%         0, 0, 1, a2*exp(-a2*a)];
-    A = [row1;
-        row2;
-        row3;
-        row4];
-    
+    row1 = [1, -1, 0, 0];
+    row2 = [-1i*a0, 0, 0, 0];
+    row3 = [0, 1, a, -exp(1i*a2*a)];
+    row4 = [0, 0, 1, -1i*a2*exp(1i*a2*a)];
 else
-    if (kappa > n0*k)
-        row1 = [1, -1, -1, 0];
-        row2 = [a0, -a1, a1, 0];
-    else
-        row1 = [1, -1, -1, 0];
-        row2 = [-a0, -a1, a1, 0];
-    end
-    
-    if (kappa > n2*k)
-        row3 = [0, exp(a1*a), exp(-a1*a), -exp(-a2*a)];
-        row4 = [0, a1*exp(a1*a), -a1*exp(-a1*a), a2*exp(-a2*a)];
-    else
-        row3 = [0, exp(a1*a), exp(-a1*a), -exp(a2*a)];
-        row4 = [0, a1*exp(a1*a), -a1*exp(-a1*a), -a2*exp(a2*a)];
-    end
-    
-    A = [row1;
-        row2;
-        row3;
-        row4];
-
+    row1 = [1, -1, -1, 0];
+    row2 = [-1i*a0, -1i*a1, 1i*a1, 0];
+    row3 = [0, exp(1i*a1*a), exp(-1i*a1*a), -exp(1i*a2*a)];
+    row4 = [0, 1i*a1*exp(1i*a1*a), -1i*a1*exp(-1i*a1*a), -1i*a2*exp(1i*a2*a)];
 end
+
+A = [row1;
+    row2;
+    row3;
+    row4];
+
 end
--- TMmatrix.m
+++ TMmatrix.m
@@ -1,5 +1,5 @@
 function A = TMmatrix(n0, n1, n2, a, k, kappa)
-% Amatrix - creates a matrix for the TE wave system
+% A - matrix for the TE wave system
 % 
 % n0 - 
 % n1 - 
@@ -8,63 +8,32 @@ function A = TMmatrix(n0, n1, n2, a, k, kappa)
 % k - wavenumber
 % kappa - frequency in the x direction
 %
-% Conley October 2013
+% Conley March 2014
 
 % tolerance
-tol = .1;
+tol = .06;
 
 % define the exponents
-a0 = sqrt(kappa^2-n0^2*k^2);
-a1 = sqrt(kappa^2-n1^2*k^2);
-a2 = sqrt(kappa^2-n2^2*k^2);
+a0 = sqrt(n0^2*k^2-kappa^2);
+a1 = sqrt(n1^2*k^2-kappa^2);
+a2 = sqrt(n2^2*k^2-kappa^2);
 
 % kappa roughly equals n1*k
-if (abs(kappa - n1*k) < tol)
-    if (kappa > n0*k)
-        row1 = [1, -1, 0, 0];
-        row2 = [a0/(n0^2), 0, 0, 0];
-    else
-        row1 = [1, -1, 0, 0];
-        row2 = [-a0/(n0^2), 0, 0, 0];
-    end
-    
-    if (kappa > n2*k)
-        row3 = [0, 1, a, -exp(-a2*a)];
-        row4 = [0, 0, 1/(n1^2), a2*exp(-a2*a)/(n2^2)];
-    else
-        row3 = [0, 1, a, -exp(-a2*a)];
-        row4 = [0, 0, 1/(n1^2), -a2*exp(a2*a)/(n2^2)];
-    end
-    
-    A = [row1;
-        row2;
-        row3;
-        row4];
-%     A = [1, -1, 0, 0;
-%         a0/(n0^2), 0, 0, 0;
-%         0, 1, a, -exp(-a2*a);
-%         0, 0, 1/(n1^2), a2*exp(-a2*a)/(n1^2)];
-    
+if (abs(kappa - n1*k) <= tol)
+    row1 = [1, -1, 0, 0];
+    row2 = [-1i*a0/(n0^2), 0, 0, 0];
+    row3 = [0, 1, a, -exp(1i*a2*a)];
+    row4 = [0, 0, 1/(n1^2), -1i*a2*exp(1i*a2*a)/(n2^2)];
 else
-    if (kappa > n0*k)
-        row1 = [1, -1, -1, 0];
-        row2 = [a0/(n0^2), -a1/(n1^2), a1/(n1^2), 0];
-    else
-        row1 = [1, -1, -1, 0];
-        row2 = [-a0/(n0^2), -a1/(n1^2), a1/(n1^2), 0];
-    end
-    
-    if (kappa > n2*k)
-        row3 = [0, exp(a1*a), exp(-a1*a), -exp(-a2*a)];
-        row4 = [0, a1*exp(a1*a)/(n1^2), -a1*exp(-a1*a)/(n1^2), a2*exp(-a2*a)/(n2^2)];
-    else
-        row3 = [0, exp(a1*a), exp(-a1*a), -exp(a2*a)];
-        row4 = [0, a1*exp(a1*a)/(n1^2), -a1*exp(-a1*a)/(n1^2), -a2*exp(a2*a)/(n2^2)];
-    end
-    
-    A = [row1;
-        row2;
-        row3;
-        row4];
+    row1 = [1, -1, -1, 0];
+    row2 = [-1i*a0/(n0^2), -1i*a1/(n1^2), 1i*a1/(n1^2), 0];
+    row3 = [0, exp(1i*a1*a), exp(-1i*a1*a), -exp(1i*a2*a)];
+    row4 = [0, 1i*a1*exp(1i*a1*a)/(n1^2), -1i*a1*exp(-1i*a1*a)/(n1^2), -1i*a2*exp(1i*a2*a)/(n2^2)];
 end
+
+A = [row1;
+    row2;
+    row3;
+    row4];
+
 end
