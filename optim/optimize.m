5a154d2a04cc2217cd8574669151ff142e6b417c
John Conley <JCC282@gmail.com>
2014-09-03 10:45:21 -0400

    Document optimize function and fix variables

--- optim/optimize.m
+++ optim/optimize.m
@@ -1,12 +1,23 @@
-function [ns, ds, a] = optimize(m, mirror, ix, ds, ns)
+function [ds, ns, a] = optimize(m, mirror, ix, ds, ns)
+% given the number of layers and starting thicknesses and refractive
+% indices of a solar cell, finds the maximum absorbance
+%
+% m - number of layers in the solar cell
+% mirror - true if layer n+1 is a perfect mirror, false otherwise
+% ix - 1 <= ix <= m is the index of the active layer
+% ds - initial values of thicknesses
+% ns - intitial values of refractive indices
+%
+% example:
+% [ds,ns,a] = optimize(2,true,2,[2,.0633],[1,1,1])
 
-dsMin = zeros(1, m) + .001;
-nsMin = zeros(1, m+1) + 1;
-minVals = cat(2, dsMin, nsMin);
+dsMin = zeros(1,m) + .001;
+nsMin = zeros(1,m+1) + 1;
+minVals = cat(2,dsMin,nsMin);
 
-dsMax = zeros(1, m) + 5;
-nsMax = zeros(1, m+1) + 5;
-maxVals = cat(2, dsMax, nsMax);
+dsMax = zeros(1,m) + 5;
+nsMax = zeros(1,m+1) + 5;
+maxVals = cat(2,dsMax,nsMax);
 
 startVal = cat(2,ds,ns);
 
@@ -14,8 +25,8 @@ startVal = cat(2,ds,ns);
 f = @(params) 1 / totalSpectralAbsorbance(m, mirror, ix, params(1:m), params(m+1:end));
 [xb, fb, flag, outp] = fmincon(f,startVal,[],[],[],[],minVals,maxVals,[],optimset('display','iter'));
 
-ns = xb(1:m);
-ds = xb(m+1:end);
+ds = xb(1:m);
+ns = xb(m+1:end);
 a = 1 / fb;
 
 end
